e030cc191ed388b70f01b61d028b5419f0c30a8a
Rojin <miyarojin557@gmail.com>
2019-04-01 10:43:16 +0545

    Image added

--- app/src/main/java/com/imageview/MainActivity.java
+++ app/src/main/java/com/imageview/MainActivity.java
@@ -2,12 +2,17 @@ package com.imageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioButton rdSmarthome, rdRajesh, rdBhuwan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rd
     }
 }
